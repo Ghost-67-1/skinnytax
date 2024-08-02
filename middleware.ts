bfc09aa9e6d10727222d4427e27b875237299ffd
Ghost-67-G <ayan@ilmiya.com>
2024-08-02 12:26:43 +0500

    Public the pages

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,21 @@
 import { authMiddleware } from '@clerk/nextjs/server';
 
 export default authMiddleware({
-  publicRoutes: ['/api/user/webhooks', '/api/subcription/webhooks', '/']
+  publicRoutes: [
+    '/api/user/webhooks',
+    '/api/subcription/webhooks',
+    '/',
+    '/blog',
+    '/capital-tax',
+    '/carried-interest',
+    '/charitable-trust',
+    '/contact',
+    '/estate-tax',
+    '/family-office',
+    '/income-tax',
+    '/pricing',
+    '/qsbs'
+  ]
 });
 
 export const config = {
