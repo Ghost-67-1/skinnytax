769f8309f0d649179d934c7adaf66694dcd1da19
Hanan Ali <hanan@geniusmindzone.com>
2024-08-07 20:19:43 +0500

    Design Financial decision

--- components/Financialinput.jsx
+++ components/Financialinput.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
 import { IoInformationCircleOutline } from "react-icons/io5";
+import Forminput from './Forminput';
 
 
 export default function Financialinput() {
     return (
-        <div>
+        <div className='Decisions-box'>
             <div className='Decisions-header'>
                 <p className='heading'>If you are unable <span><IoInformationCircleOutline /></span></p>
                 <p className='details-content'>Provide details of someone you trust to make <strong>medical decisions</strong></p>
+            </div>
+            <div className='form-box'>
+            <div className='row'>
+                <div className="col-lg-4">
+                    <Forminput/>
+                </div>
+                <div className="col-lg-4">
+                    <Forminput/>
+                </div>
+                <div className="col-lg-4">
+                    <Forminput/>
+                </div>
+                <div className="col-lg-12">
+                    <Forminput/>
+                </div>
+                <div className="col-lg-4">
+                    <Forminput/>
+                </div>
+
+                <div className="col-lg-4">
+                    <Forminput/>
+                </div>
+            </div>
             </div>
         </div>
     );
