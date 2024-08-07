f1dda12c7bcfe85b8023dcc86ff78ec4fbe997f8
MirzaAhsan1 <113511205+MirzaAhsan1@users.noreply.github.com>
2024-08-07 05:02:22 -0700

    form

--- app/form-1/page.jsx
+++ app/form-1/page.jsx
@@ -8,12 +8,27 @@ export default function Form1() {
 
                 <div className="form-dashboard-inner">
                     <div className="form-side-bar">
-                        
+
                     </div>
                     <div className="form-dashboard">
                         <div className="progress-bar-main-wrapper">
                             <FormProgressStepper />
                         </div>
+                        <div className="title-main-wrapper">
+                            <strong className="large">Information about you (S1)</strong>
+                        </div>
+                        <form action="#" className="form">
+                            <div className="form-group">
+                                <label htmlFor="true">Were you previously married?</label>
+                                <div className="toogle-btn-wrapper">
+                                    <span>yes</span>
+                                    <label class="switch">
+                                        <input type="checkbox" className="form-control" id='true' />
+                                        <span class="slider round"></span>
+                                    </label>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
