a838380bf92d5c6ea864200d45147283a9d4faf8
Hanan Ali <hanan@geniusmindzone.com>
2024-08-12 14:52:07 +0500

    Fix welcome box

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import { currentUser } from '@clerk/nextjs/server';
 import DonutChartComponent from '@/components/DonutChartComponent';
 import HalfDoughnutChart from '@/components/HalfDoughnutChart';
-import BarChart from '@/components/ReportBarChart';
-import { Col, Row } from 'react-bootstrap';
+// import BarChart from '@/components/ReportBarChart';
+// import { Col, Row } from 'react-bootstrap';
 import Headerdashboard from "@/components/dashboard/Headerdashboard"
 import { FaUser } from "react-icons/fa";
+import needMoreInfoImg from "../../assets/images/need-more-info-img.webp";
+import Image from 'next/image';
 
 async function InputForm() {
     const user = await currentUser()
@@ -39,13 +41,33 @@ async function InputForm() {
 
                 <div className='row'>
                     <div className="col-lg-12">
-                        <div className="Dashboard_boxes py-1">
-                            <h3 style={{ marginBottom: "0px" }}>
-                                Welcome To the Dashboard
-                            </h3>
-                            <p className='pb-2'>
-                                Manage your taxes efficiently with real-time insights and simplified navigation
-                            </p>
+                        <div className="Dashboard_boxes py-1 welcome-box">
+                            <div className='row'>
+                                <div className="col-lg-4">
+                                    <Image
+                                        fetchPriority="high"
+                                        decoding="async"
+                                        src={needMoreInfoImg}
+                                        alt=""
+                                        className="object__fit--contain lazy"
+                                        // srcSet="https://skinny.tax/wp-content/uploads/2023/08/hero-img-phones.png 878w, https://skinny.tax/wp-content/uploads/2023/08/hero-img-phones-275x300.png 275w, https://skinny.tax/wp-content/uploads/2023/08/hero-img-phones-768x838.png 768w, https://skinny.tax/wp-content/uploads/2023/08/hero-img-phones-480x524.png 480w"
+                                        width={"100%"}
+                                        height={958}
+                                    />
+                                </div>
+                                <div className="col-lg-8 m-auto">
+                                    <h3 style={{ marginBottom: "0px" }}>
+                                        Welcome To the Dashboard
+                                    </h3>
+                                    <p className='pb-2'>
+                                        Manage your taxes efficiently with real-time insights and simplified navigation
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div>
+
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -103,14 +125,14 @@ async function InputForm() {
                 <div className='row'>
                     <div className="col-lg-8">
                         <div className='Dashboard_boxes'>
-                        <HalfDoughnutChart />
+                            <HalfDoughnutChart />
                         </div>
                     </div>
 
                     <div className="col-lg-4">
                         <div className='Dashboard_boxes chart-box'>
-                        
-                        <DonutChartComponent />
+
+                            <DonutChartComponent />
                         </div>
                     </div>
                 </div>
